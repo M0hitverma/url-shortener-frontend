0fef8d37a7848a52a9d697681a8cdcfbe468493c
M0hitverma <mohit41verma@gmail.com>
2024-07-04 12:12:35 +0530

    Added: Loader on header

--- src/components/Header/AuthOption.jsx
+++ src/components/Header/AuthOption.jsx
@@ -19,6 +19,7 @@ const AuthOption = () => {
   const [authorized, setAuthorized] = useState(false);
   const [user, setUser] = useState({});
   const [showMenu, setShowMenu] = useState(false);
+  const [loading, setLoading] = useState(false);
   const logoutHandler = async () => {
     const response = await logoutRequest();
     if (!response || !response.ok) {
@@ -31,11 +32,13 @@ const AuthOption = () => {
     setUser({});
   };
   const firstFetch = async () => {
+    setLoading(true);
     const response = await CheckAuth();
     if (response?.ok) {
       setUser(response.data);
       setAuthorized(true);
     }
+    setLoading(false);
   };
   useEffect(() => {
     firstFetch();
@@ -54,98 +57,105 @@ const AuthOption = () => {
 
   return (
     <div>
-      <div className="md:block hidden">
-        {!authorized ? (
-          <div className="flex flex-row gap-3 ">
-            <Button
-              handler={() => {
-                router.replace("/login");
-              }}
-              heading="Login"
-            />
-            <Button
-              handler={() => {
-                router.replace("/register");
-              }}
-              heading="Sign up"
-            />
-          </div>
-        ) : (
-          <div className="flex flex-row gap-3 items-center">
-            <div className="profile w-11 h-11 rounded-full border-2 border-black relative">
-              <Image
-                src={dp}
-                className="h-[100%] w-[100%] rounded-full"
-                alt=""
-              />
-              <div className="showProfile bg-blue-100 border-2 border-blue-400 shadow-lg">
-                <h2 className="text-black font-semibold">Hi! {user.name}</h2>
-                <div className="flex flex-row gap-2 items-center">
-                  <BsLightningChargeFill className="text-orange-400 size-4" />
-                  <span>{user.links?.length}</span>
-                </div>
-              </div>
-            </div>
-
-            <Button handler={logoutHandler} heading="Log out" />
-          </div>
-        )}
-      </div>
-      <div className="md:hidden">
-        <button onClick={() => setShowMenu(!showMenu)}>
-          <IoMdMenu className="text-3xl" />
-        </button>
-        {showMenu && (
-          <div className="fixed bg-blue-100 rounded-tl-xl rounded-bl-xl right-0   z-50 ">
+      {!loading && (
+        <>
+          <div className="md:block hidden">
             {!authorized ? (
-              <div className=" grid grid-flow-row p-3 gap-3   font-semibold">
-                <button
-                  className="py-2 px-5 border-2 border-blue-300 rounded-md text-blue-400 bg-white text-start flex flex-row items-center gap-1"
-                  onClick={() => {
+              <div className="flex flex-row gap-3 ">
+                <Button
+                  handler={() => {
                     router.replace("/login");
-                    setShowMenu(false);
                   }}
-                >
-                  <LuLogIn />
-                  Login
-                </button>
-                <button
-                  className="py-2 px-5 border-2 rounded-md border-blue-300 text-blue-400 bg-white text-start flex flex-row items-center gap-1 "
-                  onClick={() => {
+                  heading="Login"
+                />
+                <Button
+                  handler={() => {
                     router.replace("/register");
-                    setShowMenu(false);
                   }}
-                >
-                  <IoMdPerson />
-                  Register
-                </button>
+                  heading="Sign up"
+                />
               </div>
             ) : (
-              <div className=" grid grid-flow-row px-1 py-2   font-semibold">
-                <div className="py-2 px-5 text-start flex flex-row items-center gap-1">
-                  <div className="w-11 h-11 rounded-full  mr-1">
+              <div className="flex flex-row gap-3 items-center">
+                <div className="profile w-11 h-11 rounded-full border-2 border-black relative">
                   <Image
-                src={dp}
-                className="h-[100%] w-[100%] rounded-full"
-                alt=""
-              />
-                    </div> <span>{user.name}</span>
+                    src={dp}
+                    className="h-[100%] w-[100%] rounded-full"
+                    alt=""
+                  />
+                  <div className="showProfile bg-blue-100 border-2 border-blue-400 shadow-lg">
+                    <h2 className="text-black font-semibold">
+                      Hi! {user.name}
+                    </h2>
+                    <div className="flex flex-row gap-2 items-center">
+                      <BsLightningChargeFill className="text-orange-400 size-4" />
+                      <span>{user.links?.length}</span>
+                    </div>
+                  </div>
                 </div>
-                <button
-                  className="py-2 px-5 text-start flex flex-row items-center gap-1 text-blue-400 "
-                  onClick={() => {
-                    logoutHandler();
-                    setShowMenu(false);
-                  }}
-                >
-                  <RiLogoutBoxLine />
-                  Log out
-                </button>
+
+                <Button handler={logoutHandler} heading="Log out" />
               </div>
             )}
           </div>
-        )}
-      </div>
+          <div className="md:hidden">
+            <button onClick={() => setShowMenu(!showMenu)}>
+              <IoMdMenu className="text-3xl" />
+            </button>
+            {showMenu && (
+              <div className="fixed bg-blue-100 rounded-tl-xl rounded-bl-xl right-0   z-50 ">
+                {!authorized ? (
+                  <div className=" grid grid-flow-row p-3 gap-3   font-semibold">
+                    <button
+                      className="py-2 px-5 border-2 border-blue-300 rounded-md text-blue-400 bg-white text-start flex flex-row items-center gap-1"
+                      onClick={() => {
+                        router.replace("/login");
+                        setShowMenu(false);
+                      }}
+                    >
+                      <LuLogIn />
+                      Login
+                    </button>
+                    <button
+                      className="py-2 px-5 border-2 rounded-md border-blue-300 text-blue-400 bg-white text-start flex flex-row items-center gap-1 "
+                      onClick={() => {
+                        router.replace("/register");
+                        setShowMenu(false);
+                      }}
+                    >
+                      <IoMdPerson />
+                      Register
+                    </button>
+                  </div>
+                ) : (
+                  <div className=" grid grid-flow-row px-1 py-2   font-semibold">
+                    <div className="py-2 px-5 text-start flex flex-row items-center gap-1">
+                      <div className="w-11 h-11 rounded-full  mr-1">
+                        <Image
+                          src={dp}
+                          className="h-[100%] w-[100%] rounded-full"
+                          alt=""
+                        />
+                      </div>{" "}
+                      <span>{user.name}</span>
+                    </div>
+                    <button
+                      className="py-2 px-5 text-start flex flex-row items-center gap-1 text-blue-400 "
+                      onClick={() => {
+                        logoutHandler();
+                        setShowMenu(false);
+                      }}
+                    >
+                      <RiLogoutBoxLine />
+                      Log out
+                    </button>
+                  </div>
+                )}
+              </div>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
